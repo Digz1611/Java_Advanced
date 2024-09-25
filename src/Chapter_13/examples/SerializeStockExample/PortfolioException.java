129c02a994540fe9d379676512c0d1ce6529f3a5
Digz1611 <diego.langeveldt@gmail.com>
2024-09-25 10:59:36 +0200

    chap 13 done

---
+++ src/Chapter_13/examples/SerializeStockExample/PortfolioException.java
@@ -0,0 +1,11 @@
+package Chapter_13.examples.SerializeStockExample;
+
+public class PortfolioException extends Exception {
+    private static final long serialVersionUID = 102L;
+    public PortfolioException(String message) {
+        super(message);
+    }
+    public PortfolioException(String message, Throwable t) {
+        super (message, t);
+    }
+}
---
+++ src/Chapter_13/examples/TestProperties.java
@@ -0,0 +1,11 @@
+package Chapter_13.examples;
+
+import java.util.Properties;
+
+public class TestProperties {
+
+    public static void main(String[] args) {
+        Properties props = System.getProperties();
+        props.list(System.out);
+    }
+}
